54d8475fa0d5fcc098ffa34fd32de1b85e4c990e
NathanVRyver <tantaouibahaabusiness@gmail.com>
2024-05-28 12:19:32 +0100

    initial

---
+++ lua/lazy_setup.lua
@@ -0,0 +1,44 @@
+
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    version = "^4",
+    import = "astronvim.plugins",
+    opts = {
+      mapleader = " ",
+      maplocalleader = ",",
+      icons_enabled = true,
+      pin_plugins = nil,
+      update_notifications = true,
+    },
+  },
+  { import = "community" },
+  { import = "plugins" },
+  {
+    "xiyaowong/nvim-transparent",
+    config = function()
+      require("transparent").setup({
+        extra_groups = { -- Makes specific groups transparent
+          "Normal", "NormalNC", "Comment", "Constant", "Special", "Identifier", "Statement", "PreProc", "Type", "Underlined", "Todo", "String", "Function", "Conditional", "Repeat", "Operator", "Structure", "LineNr", "NonText", "SignColumn", "CursorLineNr", "EndOfBuffer",
+        },
+        exclude_groups = {"tree"}, -- Exclude specific groups from being transparent
+      })
+      vim.cmd("TransparentEnable") -- Enable transparency
+    end,
+  },
+}, {
+  install = { colorscheme = { "astrodark", "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
