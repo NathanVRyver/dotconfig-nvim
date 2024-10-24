bc7a7c1c4a9d235678a5fbd46cd03ace09940554
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2024-10-24 18:24:50 +0100

    setup avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,18 +1,39 @@
 return {
-  {
-    "yetone/avante.nvim", -- Avante.nvim's repository
-    config = function()
-      require("avante").setup {
-        provider = "claude",
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-3-5-sonnet-20240620",
-          api_key = os.getenv "ANTHROPIC_API_KEY", -- Load API key from environment variable
-          temperature = 0,
-          max_tokens = 4096,
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false,
+  opts = {
+    -- opts here
+  },
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
         },
-        -- Other configuration options here...
-      }
-    end,
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
 }
